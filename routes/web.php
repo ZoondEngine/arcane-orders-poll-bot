b309a18a3375874c838b26cbb707cdf8591efed5
USER-896745 <USER-896745@DESKTOP-249DE5Q>
2024-02-09 07:55:51 +0300

    remove test routes

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/test', function() {
-    ///** @var \App\Repositories\Poll\PollRepository $repository */
-    //$repository = app(\App\Repositories\Poll\PollRepository::class);
-    //$repository->query()->get()->each->delete();
-
-    return SubscriptionScene::aggregateAvailableSubscriptions(\App\Models\TelegramUser::query()->find(1));
-});
